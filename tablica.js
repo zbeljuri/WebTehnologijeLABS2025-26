c332bb2f58d466cf9ff1080f8de5799e2e887b5f
zanabeljuri <114905511+zanabeljuri@users.noreply.github.com>
2025-11-05 15:38:54 +0100

    Ispravka

--- tablica.js
+++ tablica.js
@@ -10,11 +10,17 @@ document.write("</tr>");
 // redovi tabele
 for (let i = 1; i <= 10; i++) {
   document.write("<tr>");
-  document.write("<th>" + i + "</th>"); // zaglavlje reda
+
+  // zaglavlje reda
+  document.write("<th>" + i + "</th>");
+
+  // ćelije
   for (let j = 1; j <= 10; j++) {
     document.write("<td>" + (i * j) + "</td>");
   }
+
   document.write("</tr>");
 }
 
 document.write("</table>");
+
