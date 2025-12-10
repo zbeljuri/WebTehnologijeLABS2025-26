d5db8fbafac57a95770c8d959acb67633067bffd
zanabeljuri <114905511+zanabeljuri@users.noreply.github.com>
2025-12-10 16:06:56 +0100

    Drugi dio I

---
+++ server3.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+    console.log("Server radi na http://localhost:8080");
+});
