06a905d9769b1a2d6bd9d2cee4cf2e059fb93b17
zanabeljuri <114905511+zanabeljuri@users.noreply.github.com>
2025-12-24 11:51:20 +0100

    Regex 1. dio

---
+++ regex.js
@@ -0,0 +1,10 @@
+//Z1
+const reg1=new RegExp('\\d_..*');
+let str='3_aHello World';
+console.log(reg1.test(str)); // true
+//Z2
+const reg2=new RegExp('([0-2][1-9]|3[0-1])-(0[1-9]|1[0-2])-\\d{4}');
+let str2='12-34-2023';
+console.log(reg2.test(str2)); // false
+let str3='12-04-2023';
+console.log(reg2.test(str3)); // true
